a893a57fca4e77b5512e4ad7fc958253bdeef661
zincalex <alessandro.viesp@gmail.com>
2024-03-19 11:50:12 +0100

    Task up to 4 done (Try to make directories universal in order to avoid problem with portatile and fisso)

--- Lab1/task2/ImageFilters.h
+++ Lab1/task2/ImageFilters.h
@@ -2,9 +2,13 @@
 #define IMAGEFILTERS_H
 
 #include <opencv4/opencv2/core.hpp>
+#include<vector>
+#include<algorithm>
 
 int max_filter (const cv::Mat& src, cv::Mat& out, const int& kernel_size);
 
 int min_filter (const cv::Mat& src, cv::Mat& out, const int& kernel_size);
+
+int median_filter(const cv::Mat& src, cv::Mat& out, const int& kernel_size);
 
 #endif
--- Lab1/task2/main.cpp
+++ Lab1/task2/main.cpp
@@ -4,36 +4,85 @@
 #include <opencv4/opencv2/imgcodecs.hpp>
 #include <opencv4/opencv2/imgproc.hpp>
 #include <opencv4/opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace cv;
 
 int main(int argc, char** argv) {
-
     Mat Lena_corr_img = imread("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/Images/Lena_corrupted.png");
     Mat Astronaut_corr_img = imread("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/Images/Astronaut_salt_pepper.png");
+    Mat Garden_grayscale_img = imread("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task1/GardenModifiedGrayscale.jpg");
 
     cvtColor(Lena_corr_img, Lena_corr_img, COLOR_BGR2GRAY);
     cvtColor(Astronaut_corr_img, Astronaut_corr_img, COLOR_BGR2GRAY);
+    cvtColor(Garden_grayscale_img, Garden_grayscale_img, COLOR_BGR2GRAY);
 
-    int kernel_size = 5;
+    int KS = 3; // Kernel size
 
+    Mat Lena_modified;
+    Mat Astronaut_modified;
+    Mat Garden_modified;
 
-    if (max_filter(Lena_corr_img, Lena_corr_img, kernel_size) != 0)
-        return -1;
+    // Max or Min filter
+    if (max_filter(Lena_corr_img, Lena_modified, KS) != 0) return -1;
+    if (max_filter(Astronaut_corr_img, Astronaut_modified, KS) != 0) return -1;
+    if(max_filter(Garden_grayscale_img, Garden_modified, KS) != 0) return -1;
 
-    if (max_filter(Astronaut_corr_img, Astronaut_corr_img, kernel_size) != 0)
-        return -1;
-
-    namedWindow("Lena corrupted image with kernel size " + std::to_string(kernel_size));
-    imshow("Lena corrupted image with kernel size " + std::to_string(kernel_size), Lena_corr_img);
+    namedWindow("Lena with max/min filter");
+    imshow("Lena with max/min filter", Lena_modified);
     waitKey(0);
+    imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task2/Lena_maxmin.jpg", Lena_modified);
 
-   imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task1/Lena.jpg", Lena_corr_img);
-
-    namedWindow("Astronaut revamped image with kernel size " + std::to_string(kernel_size));
-    imshow("Astronaut revamped image with kernel size " + std::to_string(kernel_size), Astronaut_corr_img);
+    namedWindow("Astronaut with max/min filter");
+    imshow("Astronaut with max/min filter", Astronaut_modified);
     waitKey(0);
+    imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task2/Astronaut_maxmin.jpg", Astronaut_modified);
 
+    namedWindow("Garden no eletric cables max/min filter");
+    imshow("Garden no eletric cables max/min filter", Garden_modified);
+    waitKey(0);
+    imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task2/GardenNoCables_maxmin.jpg", Garden_modified);
+
+    // Median filter
+    if (median_filter(Lena_corr_img, Lena_modified, KS) != 0) return -1;
+    if (median_filter(Astronaut_corr_img, Astronaut_modified, KS) != 0) return -1;
+    if(median_filter(Garden_grayscale_img, Garden_modified, KS) != 0) return -1;
+
+    namedWindow("Lena with median filter");
+    imshow("Lena with median filter", Lena_modified);
+    waitKey(0);
+    imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task2/Lena_median.jpg", Lena_modified);
+
+    namedWindow("Astronaut with median filter");
+    imshow("Astronaut with median filter", Astronaut_modified);
+    waitKey(0);
+    imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task2/Astronaut_median.jpg", Astronaut_modified);
+
+    namedWindow("Garden no eletric cables median filter");
+    imshow("Garden no eletric cables median filter", Garden_modified);
+    waitKey(0);
+    imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task2/GardenNoCables_median.jpg", Garden_modified);
+
+
+    // Gaussian smoothing
+    GaussianBlur(Lena_corr_img, Lena_modified, Size(KS, KS), 0);
+    GaussianBlur(Astronaut_corr_img, Astronaut_modified, Size(KS, KS), 0);
+    GaussianBlur(Garden_grayscale_img, Garden_modified, Size(KS, KS), 0);
+
+    namedWindow("Lena with gauss filter");
+    imshow("Lena with gauss filter", Lena_modified);
+    waitKey(0);
+    imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task2/Lena_gauss.jpg", Lena_modified);
+
+    namedWindow("Astronaut with gauss filter");
+    imshow("Astronaut with gauss filter", Astronaut_modified);
+    waitKey(0);
+    imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task2/Astronaut_gauss.jpg", Astronaut_modified);
+
+    namedWindow("Garden no eletric cables gauss filter");
+    imshow("Garden no eletric cables gauss filter", Garden_modified);
+    waitKey(0);
+    imwrite("/home/zincalex/Uni/Computer-Vision-Laboratory/Lab1/task2/GardenNoCables_gauss.jpg", Garden_modified);
 
     return 0;
 }
