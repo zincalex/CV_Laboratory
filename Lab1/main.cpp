c001f3c73cefb6c1502b494c9cf2d42466e099e8
zincalex <alessandro.viesp@gmail.com>
2024-03-21 12:24:28 +0100

    directory changes and various main fixes

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,4 +1,4 @@
-#include "ImageFilters.h"
+#include "include/ImageFilters.h"
 #include "string"
 #include "iostream"
 
@@ -14,35 +14,41 @@ int main(int argc, char** argv)
     if (argc < 2) return -1;
     std::string PATH = argv[1]; // path to Images folder
 
-    // kernels size
-    int KS_maxORmin = 5;
-    int KS_median = 9;
-    int KS_gauss = 13;
-
-    Mat Lena_corr_img = imread(PATH + "/Lena_corrupted.png");
-    Mat Astronaut_corr_img = imread(PATH + "/Astronaut_salt_pepper.png");
-    Mat Garden_grayscale_img = imread(PATH + "/GardenModifiedGrayscale.jpg");
-
-    cvtColor(Lena_corr_img, Lena_corr_img, COLOR_BGR2GRAY);
-    cvtColor(Astronaut_corr_img, Astronaut_corr_img, COLOR_BGR2GRAY);
-    cvtColor(Garden_grayscale_img, Garden_grayscale_img, COLOR_BGR2GRAY);
+    // Task 1
+    //
+    Mat Garden_grayscale = imread(PATH + "/GardenModifiedGrayscale.jpg");
+    cvtColor(Garden_grayscale, Garden_grayscale, COLOR_BGR2GRAY);
+    namedWindow("Garden grayscale");
+    imshow("Garden grayscale", Garden_grayscale);
+    waitKey(0);
 
     // Task 2
     // Max or Min filter
+    int KS_maxORmin = 3;
+    int KS_median = 7;
+    int KS_gauss = 3;
+
+    Mat Lena_corr_img = imread(PATH + "/Lena_corrupted.png");
+    Mat Astronaut_corr_img = imread(PATH + "/Astronaut_salt_pepper.png");
+    cvtColor(Lena_corr_img, Lena_corr_img, COLOR_BGR2GRAY);
+    cvtColor(Astronaut_corr_img, Astronaut_corr_img, COLOR_BGR2GRAY);
+
+
     Mat Lena_modified = max_filter(Lena_corr_img, KS_maxORmin);
     Mat Astronaut_modified = max_filter(Astronaut_corr_img, KS_maxORmin);
-    Mat Garden_modified = max_filter(Garden_grayscale_img, KS_maxORmin);
+    Mat Garden_modified = max_filter(Garden_grayscale, KS_maxORmin);
+
     // TODO adjust path to save images
-    imwrite(PATH + "Lena_max_" + std::to_string(KS_maxORmin) + ".png", Lena_modified);
-    imwrite(PATH + "Astronaut_max_" + std::to_string(KS_maxORmin) + ".png", Astronaut_modified);
-    imwrite(PATH + "Garden_max_" + std::to_string(KS_maxORmin) + ".jpg", Garden_modified);
+    //imwrite(PATH + "Lena_max_" + std::to_string(KS_maxORmin) + ".png", Lena_modified);
+    //imwrite(PATH + "Astronaut_max_" + std::to_string(KS_maxORmin) + ".png", Astronaut_modified);
+    //imwrite(PATH + "Garden_max_" + std::to_string(KS_maxORmin) + ".jpg", Garden_modified);
 
 
     // Task 3
     // Median filter
     Lena_modified = median_filter(Lena_corr_img, KS_median);
     Astronaut_modified = median_filter(Astronaut_corr_img, KS_median);
-    Garden_modified = median_filter(Garden_grayscale_img, KS_median);
+    Garden_modified = median_filter(Garden_grayscale, KS_median);
 
     imwrite(PATH + "Lena_median_" + std::to_string(KS_median) + ".png", Lena_modified);
     imwrite(PATH + "Astronaut_median_" + std::to_string(KS_median) + ".png", Astronaut_modified);
@@ -53,11 +59,11 @@ int main(int argc, char** argv)
     // Gaussian smoothing
     GaussianBlur(Lena_corr_img, Lena_modified, Size(KS_gauss, KS_gauss), 0);
     GaussianBlur(Astronaut_corr_img, Astronaut_modified, Size(KS_gauss, KS_gauss), 0);
-    GaussianBlur(Garden_grayscale_img, Garden_modified, Size(KS_gauss, KS_gauss), 0);
+    GaussianBlur(Garden_grayscale, Garden_modified, Size(KS_gauss, KS_gauss), 0);
 
-    imwrite(PATH + "Lena_gauss.png", Lena_modified);
-    imwrite(PATH + "Astronaut_gauss.png", Astronaut_modified);
-    imwrite(PATH + "Garden_gauss.jpg", Garden_modified);
+    //imwrite(PATH + "Lena_gauss_" + std::to_string(KS_gauss) + ".png", Lena_modified);
+    //imwrite(PATH + "Astronaut_gauss_" + std::to_string(KS_gauss) + ".png", Astronaut_modified);
+    //imwrite(PATH + "Garden_gauss_" + std::to_string(KS_gauss) + ".jpg", Garden_modified);
 
 
     // Task 5
@@ -66,11 +72,12 @@ int main(int argc, char** argv)
     int histSize = 256;
     float range[] = {0, 256};
     const float* histRange[] = {range};
-    calcHist(&Garden_grayscale_img, 1, 0, Mat(), hist, 1, &histSize, histRange, true, false);
+    calcHist(&Garden_grayscale, 1, 0, Mat(), hist, 1, &histSize, histRange, true, false);
     int hist_width = 1024;
     int hist_height = 800;
     int bin_w = cvRound((double) hist_width / histSize);
     Mat hist_img(hist_height, hist_width, CV_8UC3, Scalar(0,0,0));
+    normalize(hist, hist, 0, hist_img.rows, NORM_MINMAX, -1, Mat());
     for(int i = 1; i < histSize; i++)
     {
         line(hist_img, Point(bin_w * (i - 1), hist_height - cvRound(hist.at<float>(i - 1))), Point(bin_w * (i), hist_height - cvRound(hist.at<float>(i))), Scalar(255, 255, 255), 2, 8, 0);
@@ -84,21 +91,19 @@ int main(int argc, char** argv)
     // Task 6
     // Histogram equalization
     Mat	GardenImg_equalized;
-    equalizeHist(Garden_grayscale_img, GardenImg_equalized);
+    equalizeHist(Garden_grayscale, GardenImg_equalized);
 
-    namedWindow("Garden image equalized");
-    imshow("Garden image equalized", GardenImg_equalized);
-    waitKey(0);
     imwrite(PATH + "/GardenImg_equalized.jpg", GardenImg_equalized);
 
     Mat hist_equalized;
     Mat histImage_equalized(hist_height, hist_width, CV_8UC3, cv::Scalar(0, 0, 0));
     calcHist(&GardenImg_equalized, 1, 0, cv::Mat(), hist_equalized, 1, &histSize, histRange, true, false);
-    normalize(hist_equalized, hist_equalized, 0, histImage_equalized.rows, cv::NORM_MINMAX, -1, cv::Mat());
+    normalize(hist_equalized, hist_equalized, 0, histImage_equalized.rows, NORM_MINMAX, -1, Mat());
     for (int i = 1; i < histSize; i++)
     {
         line(histImage_equalized, Point(bin_w * (i - 1), hist_height - cvRound(hist_equalized.at<float>(i - 1))), Point(bin_w * (i), hist_height - cvRound(hist_equalized.at<float>(i))), Scalar(255, 255, 255), 2, 8, 0);
     }
+
     imshow("histogram equalized", histImage_equalized);
     waitKey(0);
     imwrite(PATH + "/histogram_equalized.jpg", histImage_equalized);
--- Lab1/src/ImageFilters.cpp
+++ Lab1/src/ImageFilters.cpp
@@ -1,4 +1,4 @@
-#include "ImageFilters.h"
+#include "../include/ImageFilters.h"
 
 cv::Mat max_filter(const cv::Mat& src, const int& kernel_size)
 {
@@ -7,17 +7,17 @@ cv::Mat max_filter(const cv::Mat& src, const int& kernel_size)
     if(src.channels() != 1) throw std::invalid_argument("Error: the image provided must have only one channel");
 
     cv::Mat out(src.rows, src.cols, CV_8U);
-    for(int i = 0; i < src.rows; i += kernel_size) // filter
+    for(int i = 0; i < src.rows; i++)
     {
-        for(int j = 0; j < src.cols; j += kernel_size)
+        for(int j = 0; j < src.cols; j++)
         {
             int max = 0;
             int temp;
-            for(int x = 0; x < kernel_size; x++)
+            for(int x = -kernel_size/2; x <= kernel_size/2; x++)
             {
-                for(int y = 0; y < kernel_size; y++)
+                for(int y = -kernel_size/2; y <= kernel_size/2; y++)
                 {
-                    if((i+x) < src.rows && (j+y) < src.cols) // in case the kernel exceeds the image size
+                    if((i+x) >= 0 && (j+y) >= 0 && (i+x) < src.rows && (j+y) < src.cols) // in case the kernel exceeds the image size
                     {
                         temp = src.at<unsigned char> (i+x, j+y);
                         if(temp > max)
@@ -26,9 +26,9 @@ cv::Mat max_filter(const cv::Mat& src, const int& kernel_size)
                 }
             }
 
-            for(int x = 0; x < kernel_size; x++)
-                for(int y = 0; y < kernel_size; y++)
-                    if((i+x) < src.rows && (j+y) < src.cols)
+            for(int x = -kernel_size/2; x <= kernel_size/2; x++)
+                for(int y = -kernel_size/2; y <= kernel_size/2; y++)
+                    if((i+x) >= 0 && (j+y) >= 0 && (i+x) < src.rows && (j+y) < src.cols)
                         out.at<unsigned char> (i+x, j+y) = max;
 
         }
@@ -43,17 +43,17 @@ cv::Mat min_filter(const cv::Mat& src, const int& kernel_size)
     if(src.channels() != 1) throw std::invalid_argument("Error: the image provided must have only one channel");
 
     cv::Mat out(src.rows, src.cols, CV_8U);
-    for(int i = 0; i < src.rows; i += kernel_size) // filter
+    for(int i = 0; i < src.rows; i++) // filter
     {
-        for(int j = 0; j < src.cols; j += kernel_size)
+        for(int j = 0; j < src.cols; j++)
         {
             int min = 255;
             int temp;
-            for(int x = 0; x < kernel_size; x++)
+            for(int x = -kernel_size/2; x <= kernel_size/2; x++)
             {
-                for(int y = 0; y < kernel_size; y++)
+                for(int y = -kernel_size/2; y <= kernel_size/2; y++)
                 {
-                    if((i+x) < src.rows && (j+y) < src.cols)
+                    if((i+x) >= 0 && (j+y) >= 0 && (i+x) < src.rows && (j+y) < src.cols) // in case the kernel exceeds the image size
                     {
                         temp = src.at<unsigned char> (i+x, j+y);
                         if(temp < min)
@@ -62,9 +62,9 @@ cv::Mat min_filter(const cv::Mat& src, const int& kernel_size)
                 }
             }
 
-            for(int x = 0; x < kernel_size; x++)
-                for(int y = 0; y < kernel_size; y++)
-                    if((i+x) < src.rows && (j+y) < src.cols)
+            for(int x = -kernel_size/2; x <= kernel_size/2; x++)
+                for(int y = -kernel_size/2; y <= kernel_size/2; y++)
+                    if((i+x) >= 0 && (j+y) >= 0 && (i+x) < src.rows && (j+y) < src.cols)
                         out.at<unsigned char> (i+x, j+y) = min;
         }
     }
@@ -78,28 +78,27 @@ cv::Mat median_filter(const cv::Mat& src, const int& kernel_size)
     if(src.channels() != 1) throw std::invalid_argument("Error: the image provided must have only one channel");
 
     cv::Mat out(src.rows, src.cols, CV_8U);
-    for(int i = 0; i < src.rows; i += kernel_size) // filter
+    for(int i = 0; i < src.rows; i++) // filter
     {
-        for(int j = 0; j < src.cols; j += kernel_size)
+        for(int j = 0; j < src.cols; j++)
         {
             std::vector<int> kernel_elements;
-            for(int x = 0; x < kernel_size; x++)
+            for(int x = -kernel_size/2; x <= kernel_size/2; x++)
             {
-                for(int y = 0; y < kernel_size; y++)
+                for(int y = -kernel_size/2; y <= kernel_size/2; y++)
                 {
-                    if((i+x) < src.rows && (j+y) < src.cols)  // In case the kernel exceeds the image size
+                    if((i+x) >= 0 && (j+y) >= 0 && (i+x) < src.rows && (j+y) < src.cols) // in case the kernel exceeds the image size
                         kernel_elements.insert(kernel_elements.end(), src.at<unsigned char> (i+x, j+y));
+
                 }
             }
 
             // Sorting and Median
             std::sort(kernel_elements.begin(), kernel_elements.end());
-            unsigned long numElem_kernel = kernel_elements.size();
-            unsigned char median = kernel_elements[(numElem_kernel/ 2) + 1]; // always odd elements inside the vector
-
-            for(int x = 0; x < kernel_size; x++)
-                for(int y = 0; y < kernel_size; y++)
-                    if((i+x) < src.rows && (j+y) < src.cols)
+            unsigned char median = kernel_elements[int(kernel_elements.size()/ 2)]; // always odd elements inside the vector
+            for(int x = -kernel_size/2; x <= kernel_size/2; x++)
+                for(int y = -kernel_size/2; y <= kernel_size/2; y++)
+                    if((i+x) >= 0 && (j+y) >= 0 && (i+x) < src.rows && (j+y) < src.cols)
                         out.at<unsigned char> (i+x, j+y) = median;
 
         }
