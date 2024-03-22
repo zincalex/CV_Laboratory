4451c22c078ec4b52572d88750980c3a1d46e73f
zincalex <alessandro.viesp@gmail.com>
2024-03-22 11:21:18 +0100

    almost done

---
+++ Filters_and_Histograms/cmake-build-debug/CMakeFiles/Lab2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2.dir/main.cpp.o"
+  "CMakeFiles/Lab2.dir/main.cpp.o.d"
+  "CMakeFiles/Lab2.dir/src/ImageFilters.cpp.o"
+  "CMakeFiles/Lab2.dir/src/ImageFilters.cpp.o.d"
+  "Lab2"
+  "Lab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
