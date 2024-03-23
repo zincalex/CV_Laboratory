f8c26ae730b7b058d3e0cecb8c6f8f3858bfebc7
zincalex <alessandro.viesp@gmail.com>
2024-03-23 12:33:57 +0100

    Documentation in ImageFilters.h

--- Filters_and_Histograms/include/ImageFilters.h
+++ Filters_and_Histograms/include/ImageFilters.h
@@ -5,10 +5,33 @@
 #include <vector>
 #include <algorithm>
 
+/**
+ * @brief 	Max filter is applied to the image
+ * @param 	src  input image
+ * @param 	kernel_size length of the side of a square kernel
+ * @return  filtered image
+ * @throw 	error if size is even or the number of channels of src differs from 1
+ */
 cv::Mat maxFilter (const cv::Mat& src, const int& kernel_size);
 
+
+/**
+ * @brief 	Min filter is applied to the image
+ * @param 	src  input image
+ * @param 	kernel_size length of the side of a square kernel
+ * @return  filtered image
+ * @throw 	error if size is even or the number of channels of src differs from 1
+ */
 cv::Mat minFilter (const cv::Mat& src, const int& kernel_size);
 
+
+/**
+ * @brief 	Median filter is applied to the image
+ * @param 	src  input image
+ * @param 	kernel_size length of the side of a square kernel
+ * @return  filtered image
+ * @throw 	error if size is even or the number of channels of src differs from 1
+ */
 cv::Mat medianFilter(const cv::Mat& src, const int& kernel_size);
 
 #endif
